727b4a843a0ebeb63a09a0cc413127d11c10216e
Matthias Schlögl <schloegl@fbmtpc42.tugraz.at>
2018-02-08 09:49:21 +0100

    .

--- matlab/ifft2c.m
+++ matlab/ifft2c.m
@@ -4,6 +4,6 @@ res = zeros(size(x));
 
 for i = 1:size(x,3) 
     temp = x(:,:,i);
-    res(:,:,i) = sqrt(length(temp(:))).*ifftshift(ifft2(fftshift(temp)));
+    res(:,:,i) = sqrt(length(temp(:))).*fftshift(ifft2(ifftshift(temp)));
 end
 
